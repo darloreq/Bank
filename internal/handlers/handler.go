8628243d8ba9b7c30c0f9b2381a21d2364191095
Your Name <you@example.com>
2024-09-16 20:21:10 +0500

    handlers

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,1 +1,14 @@
 package handlers
+
+import "net/http"
+
+type handler struct {
+}
+
+func New() *handler {
+	return &handler{}
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
