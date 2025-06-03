cfffad0f6eb56b18f8a5cadf8b9a5e695bc79f6c
unknown <yasha201@ya.ru>
2025-06-03 15:19:12 +0500

    test NewUser

--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.22
 
 require (
 	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.1 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 )
--- internal/handlers/handlerNewUser_test.go
+++ internal/handlers/handlerNewUser_test.go
@@ -1,1 +1,47 @@
 package handlers
+
+import (
+	"coolBank/internal/entity"
+	mock_bank "coolBank/internal/services/bank/mocks"
+	"github.com/golang/mock/gomock"
+	"net/http"
+	"testing"
+)
+
+func TestHandler_NewUser(t *testing.T) {
+	type mockBehavior func(m *mock_bank.MockBankReposI, user entity.User)
+
+	testTable := []struct {
+		name                 string
+		inputBody            string
+		inputUser            entity.User
+		mockBehavior         mockBehavior
+		expectedStatusCode   int
+		expectedResponseBody string
+	}{
+		{
+			name:      "success",
+			inputBody: `{"name": "Test"'}`,
+			inputUser: entity.User{
+				Name: "Test",
+				Balance: 0,
+
+			},
+			mockBehavior: func(m *mock_bank.MockBankReposI, user entity.User) {
+				m.EXPECT().MakeUser(gomock.Any()).Return(user, nil),
+			},
+			expectedStatusCode: http.StatusOK,
+			expectedResponseBody: `{"name":"Test"}`,
+		},
+	}
+
+	for _, tt := range testTable {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			newUser := mock_bank.NewMockBankReposI(ctrl)
+			tt.mockBehavior(newUser, tt.inputUser)
+		})
+	}
+}
