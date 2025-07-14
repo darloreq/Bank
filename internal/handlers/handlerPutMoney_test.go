07f0b3f199a01c8db7f79963354178d666ee92f0
darlore qqq <yasha201@ya.ru>
2025-07-14 21:28:06 +0500

    put money handler test

--- internal/handlers/handlerPutMoney_test.go
+++ internal/handlers/handlerPutMoney_test.go
@@ -4,7 +4,6 @@ import (
 	"coolBank/internal/entity"
 	bank "coolBank/internal/handlers/mocks"
 	"encoding/json"
-	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
@@ -18,7 +17,7 @@ func TestHandler_PutMoneyIn(t *testing.T) {
 	type mockBehavior func(m *bank.MockHeadHandler)
 	testTable := []struct {
 		name                 string
-		userID               int
+		userID               string
 		body                 amount
 		bankUseCase          mockBehavior
 		expectedStatusCode   int
@@ -29,7 +28,7 @@ func TestHandler_PutMoneyIn(t *testing.T) {
 			bankUseCase: func(m *bank.MockHeadHandler) {
 				m.EXPECT().ChangeBalance(0, entity.ChangeBalance{Amount: 100}, "put").Return(entity.Balance{Numbers: 100}, nil)
 			},
-			userID: 0,
+			userID: "0",
 			body: amount{
 				OperationType: "put",
 				TotalChange:   100,
@@ -38,26 +37,12 @@ func TestHandler_PutMoneyIn(t *testing.T) {
 			expectedResponseBody: entity.Balance{Numbers: 100},
 		},
 		{
-			name: "success take money",
+			name: "wrong ID type",
 			bankUseCase: func(m *bank.MockHeadHandler) {
-				m.EXPECT().ChangeBalance(0, entity.ChangeBalance{Amount: 100}, "take").Return(entity.Balance{Numbers: 0}, nil)
 			},
-			userID: 0,
+			userID: "qwe",
 			body: amount{
 				OperationType: "take",
-				TotalChange:   100,
-			},
-			expectedStatusCode:   http.StatusOK,
-			expectedResponseBody: entity.Balance{Numbers: 0},
-		},
-		{
-			name: "wrong operation type", //ожидаем тейк, но приходит пут. Хендлер должен отдать нам бедРеквест, но вылетает ошибка
-			bankUseCase: func(m *bank.MockHeadHandler) {
-				m.EXPECT().ChangeBalance(0, entity.ChangeBalance{Amount: 100}, "take").Return(entity.Balance{Numbers: 0}, http.StatusBadRequest)
-			},
-			userID: 0,
-			body: amount{
-				OperationType: "put",
 				TotalChange:   100,
 			},
 			expectedStatusCode:   http.StatusBadRequest,
@@ -88,7 +73,7 @@ func TestHandler_PutMoneyIn(t *testing.T) {
 			//Test Request
 			w := httptest.NewRecorder()
 
-			req, err := http.NewRequest(http.MethodPut, "/"+fmt.Sprintf("%d", tt.userID), strings.NewReader(string(requestBody)))
+			req, err := http.NewRequest(http.MethodPut, "/"+tt.userID, strings.NewReader(string(requestBody)))
 			if err != nil {
 				t.Fatal(err)
 				return
